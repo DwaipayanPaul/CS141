ba18e3e8c25dc964509f6b8bb7835a0ea8eee952
DwaipayanPaul <42205440+DwaipayanPaul@users.noreply.github.com>
2018-08-17 11:24:23 +0530

    Add files via upload
    lab assignment3 - question 1 and 2

---
+++ lab3_q1.cpp
@@ -0,0 +1,19 @@
+//Include Library
+#include<iostream>
+using namespace std;
+//Write main function
+int main() {
+//Declaring values of all types
+ int a;
+ float b;
+ char c;
+ double d;
+ bool e;
+ //Displaying sizes of the data types
+ cout<< "Size of integer a="<<sizeof(a)<<endl;
+ cout<<"Size of b="<<sizeof(b)<<endl;
+ cout<<"Size of character c="<<sizeof(c)<<endl;
+ cout<<"Size of double d="<<sizeof(d)<<endl;
+ cout<<"Size of boolean e="<<sizeof(e)<<endl;
+ return 0;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,24 @@
+//Include liibrary
+#include<iostream>
+using namespace std;
+//Write main function
+int main(){
+ int a=1,b=2;
+ float c=7.9,d=10.22;
+ //Add two numbers display
+ cout<<"sum of a and b="<<a+b<<endl;
+ cout<<"sum of c and d="<<c+d<<endl;
+ //Substract two numbers display
+ cout<<"subtraction of a and b="<<a-b<<endl;
+ cout<<"subtraction of c and d="<<c-d<<endl;
+ //Multiply two numbers and display
+ cout<<"product of a and b="<<a*b<<endl;
+ cout<<"product of c and d="<<c*d<<endl;
+ //divide two numbers and display
+ cout<<"division of a and b="<<a/b<<endl;
+ cout<<"division of c and d="<<c/d<<endl;
+ //Modulus two numbers and display
+ cout<<"modulus of a and b="<<a%b<<endl;
+return 0;
+}
+ 
